6e40e8210540167cc146432159f8139d1f0e3681
developmentd420 <development.d420@gmail.com>
2023-11-12 21:14:53 +0600

    Landing page design completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/lib/providers/next-theme-provider";
 import "@/lib/supabase/db";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -17,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
